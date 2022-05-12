a06ab75f449777357078e2d70d72b4d6b518acc9
waltergabrieltell <walterdjv@gmail.com>
2022-05-12 17:58:27 -0300

    Homework 3_12 de Mayo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -120,12 +120,22 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero:
 
-  switch (numero){
-    case numero %3 === 0: return "fizz";
-    case numero %5 === 0: return "buzz";
-    case numero %15 === 0:  return "fizzbuzz";
-    default: return numero;
-  }
+if (numero%15 === 0){
+  return "fizzbuzz";
+}else if(numero%3 === 0){
+  return "fizz";
+}else if(numero%5 ===0){
+  return "buzz";
+}else{
+  return numero;
+}
+
+  // switch (numero){
+  //   case numero %15 === 0: return "fizzbuzz";
+  //   case numero %3 === 0: return "fizz";
+  //   case numero %5 === 0:  return "buzz";
+  //   default: return numero;
+  // }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -175,25 +185,49 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
+  if (numero == 0 || numero == 1) return false;
+  for (var i = 2; i < numero/2; i++) {
+    if (numero % i == 0){
+      return false;
+    } else{
+      return true;
+    }
+  
 }
+
+}
+
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
+  return valor? "Soy verdadero" : "Soy falso";
+
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
+  //Escribe tu código aquí:
+  
+  var arreglo=[];
+  for (var i = 0; i < 11; i++) {
+//   arreglo[i]=i*6;
+  arreglo.push(i*6);  
+  }
+  return arreglo;
   
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
-  //Escribe tu código aquí
+  //Escribe tu código aquí:
+
+  return numero  >  99  &&  numero  <  1000? true : false;
   
 }
 
@@ -201,7 +235,20 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+
+var suma = numero;
+var i=0;
+
+do{
+  i++
+  suma = suma +5;
+  
+}while(i<8)
+return suma;
+
 }
+
+
 
 
 // No modificar nada debajo de esta línea
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,26 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+
+  return array[0];
+
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+
+  return array[array.length-1]; 
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+
+return array.length;
+
 }
 
 
@@ -23,6 +31,12 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+
+//var array = [];
+for (i=0; i<array.length; i++){
+  array[i]= array[i]+1;
+}return array;
+
 }
 
 
@@ -30,6 +44,11 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+
+
+  array.push(elemento);
+  return array;
+
 }
 
 
@@ -38,6 +57,9 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -47,6 +69,17 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+
+  var cadena="";
+  for(var i=0; i<palabras.length; i++){
+    if(i==palabras.length-1){
+      cadena=cadena + palabras[i];
+    }else{
+      cadena=cadena + palabras[i]+ " ";
+    }
+    
+  }return cadena;
+
 }
 
 
