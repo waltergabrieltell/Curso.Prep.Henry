e410c38846598923225d3bf1501c52c9fb2d526e
waltergabrieltell <walterdjv@gmail.com>
2022-05-27 18:02:04 -0300

    tarea10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,17 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var  nuevoArray=[];
+  var i=0;
+  nuevoArray = Object.entries(objeto);
+
+  //   for (var clave in objeto) {
+  //     var j=0;
+  //   nuevoArray[i][j]=clave;
+  //   nuevoArray[i][j+1]=objeto[clave];
+  //   i++;
+  // }
+  return nuevoArray;
 }
 
 
@@ -18,6 +29,9 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+
+  for(va)
+
 }
 
 
